d98d089bbea20423bcbc3ea4ed0029bd853d726a
Natawon <32977030+Natawon@users.noreply.github.com>
2019-01-17 17:49:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@
 #docker rm pat
 #docker rmi pat
 docker build -t pat .
-docker run --name pat -v $PWD/apps:/var/www/html -p 80:80 -h lemp -d pat:latest
-docker exec -ti pat bash
+#docker run --name pat -v $PWD/apps:/var/www/html -p 80:80 -h lemp -d pat:latest
+#docker exec -ti pat bash
 
